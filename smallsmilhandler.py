fad13fe9c4ca8e59e50b15abe3a0fe17eabacb73
Pablo Castro Ilundain <pilundai@gamma07.aulas.gsyc.urjc.es>
2017-10-16 14:33:53 +0200

    Corregido con Pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,15 +4,20 @@
 from xml.sax.handler import ContentHandler
 import sys
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
         """
         Constructor. Inicializamos las variables
         """
         self.list_tags = []
-        self.tags = {'root-layout': ['width', 'height', 'background-color'], 'region': ['id', 'top', 'bottom', 'left', 'right'], 'img': ['src', 'region', 'begin', 'dur'], 'audio': ['src', 'begin', 'dur'], 'textstream': ['src', 'region']}
-                    
+        self.tags = {'root-layout': ['width', 'height', 'background-color'],
+                     'region': ['id', 'top', 'bottom', 'left', 'right'],
+                     'img': ['src', 'region', 'begin', 'dur'],
+                     'audio': ['src', 'begin', 'dur'],
+                     'textstream': ['src', 'region']}
+
     def startElement(self, name, attrs):
         """
         Método para cuando se abre una etiqueta SMIL
@@ -23,7 +28,7 @@ class SmallSMILHandler(ContentHandler):
             for elem in self.tags[name]:
                 diccionario[elem] = attrs.get(elem, "")
             self.list_tags.append(diccionario)
-            
+
     def get_tags(self):
         """
         Devuelve la lista
