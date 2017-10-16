d621a1e4b0d04ee31a0b9901585855ea3a49a284
Pablo Castro Ilundain <pilundai@gamma06.aulas.gsyc.urjc.es>
2017-10-16 09:48:02 +0200

    Versión orientada a objetos de karaoke.py

--- karaoke.py
+++ karaoke.py
@@ -8,35 +8,52 @@ import smallsmilhandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-if __name__ == '__main__':
-    """
-    Programa principal
-    """
-    ssHandler = smallsmilhandler.SmallSMILHandler()
-    taglist = ssHandler.get_tags()
-    parser = make_parser()
-    parser.setContentHandler(ssHandler)
-    try:
-        parser.parse(open(sys.argv[1]))
-    except IndexError:
-        sys.exit("Usage: python3 karaoke.py file.smil")
-          
-    for dic in taglist:
-        print(dic['tag'] + '\t', end='')
+class KaraokeLocal
+
+    def __init__ (self, fsmil):
+        ssHandler = smallsmilhandler.SmallSMILHandler()
+        self.taglist = ssHandler.get_tags()
+        parser = make_parser()
+        parser.setContentHandler(ssHandler)
+        parser.parse(fmsil)
+        
+    def __str__ (self)
+        for dic in self.taglist:
+            print(dic['tag'] + '\t', end='')
         for atribs in dic:
             if atribs == 'tag':
                 pass
             else:
                 print(atribs + '=' + dic[atribs] + '\t', end='')
-                if atribs == 'src':
-                    try:
-                        urllib.request.urlretrieve(dic[atribs], dic[atribs][dic[atribs].rfind('/') + 1:])
-                    except ValueError:
-                        pass
-        print('\n')
+            print('\n')
+            
+    def to_json (self, fsmil, fjson)
+        if fjson == '':
+            fjson == fsmil.split('.')[0] + '.json'
+        json.dump(self.taglist, open(fjson, 'w'))
+              
+    def do_local (self)
+        for dic in self.taglist:
+            for atribs in dic:
+                if atribs == 'src' and 'http' in dic[atribs]:
+                    urllib.request.urlretrieve(dic[atribs], dic[atribs][dic[atribs].rfind('/') + 1:])
+                    dic[atribs] = dic[atribs][dic[atribs].rfind('/') + 1:]
+
+if __name__ == '__main__':
+    
+    try:
+        __init__(sys.argv[1])
+    except IndexError:
+            sys.exit("Usage: python3 karaoke.py file.smil")
+
+
+
+
+
+            
 
                     
 
 
-    json.dump(taglist, open('karaoke.json', 'w'))
+
     
